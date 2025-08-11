c135766f530997b43aea572d20cd56efb1d24952
k88936 <k88936@TeamCity>
2025-08-11 17:22:11 +0000

    artifact dependencies of 'Deploy' build configuration were updated (TeamCity change in 'Source Of / DeployPage' project)

--- root/patches/buildTypes/SourceOf_Deploy.kts
+++ root/patches/buildTypes/SourceOf_Deploy.kts
@@ -43,7 +43,7 @@ create(RelativeId("SourceOf_MyCollection"), BuildType({
     dependencies {
         artifacts(RelativeId("SourceOf_Build")) {
             buildRule = lastSuccessful()
-            artifactRules = "*"
+            artifactRules = "* => /"
         }
     }
 }))
