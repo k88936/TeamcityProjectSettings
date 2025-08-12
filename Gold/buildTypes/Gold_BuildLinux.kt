061a4d07a172c619f2d6978cbf038b4239943e39
k88936 <k88936@TeamCity>
2025-08-12 18:36:15 +0000

    VCS roots of 'Build_linux' build configuration were updated (TeamCity change in 'Gold' project)

--- Gold/buildTypes/Gold_BuildLinux.kt
+++ Gold/buildTypes/Gold_BuildLinux.kt
@@ -4,4 +4,8 @@ import jetbrains.buildServer.configs.kotlin.*
 
 object Gold_BuildLinux : BuildType({
     name = "Build_linux"
+
+    vcs {
+        root(Gold.vcsRoots.Gold_GitGithubComK88936goldGitRefsHeadsMain)
+    }
 })
