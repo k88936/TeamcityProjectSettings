dbe0feda69afbb95902f750af2de49f7c7be9986
kvtodev <kvtodev@outlook.com>
2025-08-13 20:44:55 +0800

    add Qt Windows build configuration template for Shotmd application

--- Shotmd/buildTypes/Shotmd_Build.kt
+++ Shotmd/buildTypes/Shotmd_Build.kt
@@ -1,52 +1,19 @@
 package Shotmd.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import _Self.utils.DeploymentBuilders
 
 object Shotmd_Build : BuildType({
-    name = "Build"
-
-    artifactRules = "build/Shotmd.zip"
-
+    DeploymentBuilders.createQtWindowsBuild(
+        name = "Build",
+        qtInstallPath = "C:/Qt/6.9.1/msvc2022_64",
+        buildConfig = "Release",
+        executableName = "Shotmd"
+    )(this)
+    
     vcs {
         root(Shotmd.vcsRoots.Shotmd_GitGithubComK88936ShotmdGitRefsHeadsMaster)
-    }
-
-    params {
-        text("env.QT_INSTALL", "C:/Qt/6.9.1/msvc2022_64")
-    }
-
-    steps {
-        powerShell {
-            id = "jetbrains_powershell"
-            scriptMode = script {
-                content = """
-                    cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%env.QT_INSTALL%/lib/cmake/Qt6/qt.toolchain.cmake -G Ninja 
-                    cmake --build build --config Release
-                    mkdir build/Shotmd
-                    cp build/Shotmd.exe build/Shotmd
-                    %env.QT_INSTALL%/bin/windeployqt.exe build/Shotmd/Shotmd.exe
-                    cd build
-                    7z a Shotmd.zip Shotmd/
-                """.trimIndent()
-            }
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-
-    requirements {
-        exists("env.QT")
-        exists("env.PLATFORM_WIN")
     }
 })
--- _Self/utils/DeploymentBuilders.kt
+++ _Self/utils/DeploymentBuilders.kt
@@ -2,8 +2,53 @@ package _Self.utils
 
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 object DeploymentBuilders {
+    
+    /**
+     * 创建一个构建 Qt Windows 应用程序的构建类型
+     */
+    fun createQtWindowsBuild(
+        name: String = "Build",
+        qtInstallPath: String = "C:/Qt/6.9.1/msvc2022_64",
+        buildConfig: String = "Release",
+        executableName: String = "Application"
+    ): BuildType.() -> Unit {
+        return {
+            this.name = name
+            this.artifactRules = "build/${executableName}.zip"
+            
+            params {
+                text("env.QT_INSTALL", qtInstallPath)
+            }
+            
+            steps {
+                powerShell {
+                    id = "jetbrains_powershell"
+                    scriptMode = script {
+                        content = """
+                            cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%env.QT_INSTALL%/lib/cmake/Qt6/qt.toolchain.cmake -G Ninja 
+                            cmake --build build --config $buildConfig
+                            rm -r build/${executableName}
+                            mkdir build/${executableName}
+                            cp build/${executableName}.exe build/${executableName}
+                            %env.QT_INSTALL%/bin/windeployqt.exe build/${executableName}/${executableName}.exe
+                            cd build
+                            7z a ${executableName}.zip ${executableName}/
+                        """.trimIndent()
+                    }
+                }
+            }
+            
+            requirements {
+                exists("env.QT")
+                exists("env.PLATFORM_WIN")
+            }
+        }
+    }
     
     /**
      * 创建一个GitHub Release部署构建类型
