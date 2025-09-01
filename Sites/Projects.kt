319fb18cbe6d517bf8fd3afffd60e10d4b591b69
kvtodev <kvtodev@outlook.com>
2025-09-01 11:32:14 +0800

    update

--- Sites/Projects.kt
+++ Sites/Projects.kt
@@ -14,7 +14,7 @@ object Projects : Project({
             password("env.S3_SECRET_KEY", "credentialsJSON:6d9f4af1-89b3-41a3-9bcc-fde1bdd8e7f9")
             param("env.S3_BUCKET_NAME", "software-release")
             param("env.S3_ENDPOINT", "http://10te47kl27611.vicp.fun:19000")
-            param("env.S3_AWS_REGION", "us-east-1")
+            param("env.AWS_REGION", "us-east-1")
             password("env.S3_ACCESS_KEY", "credentialsJSON:e2bf46ef-fcae-472d-a934-117a88db5241")
         }
     })
