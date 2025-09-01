2567ef087e2524dd24f20aad722b30bc665ad084
kvtodev <kvtodev@outlook.com>
2025-09-01 11:24:44 +0800

    update

--- Sites/Projects.kt
+++ Sites/Projects.kt
@@ -11,11 +11,11 @@ object Projects : Project({
     subProject(GithubPageTemplate("git@github.com:k88936/k88936.github.io.git"))
     subProject(GithubPageTemplate("git@github.com:k88936/source-of.git") {
         it.params {
-            password("env.AWS_SECRET_KEY", "credentialsJSON:6d9f4af1-89b3-41a3-9bcc-fde1bdd8e7f9")
+            password("env.S3_SECRET_KEY", "credentialsJSON:6d9f4af1-89b3-41a3-9bcc-fde1bdd8e7f9")
             param("env.S3_BUCKET_NAME", "software-release")
             param("env.S3_ENDPOINT", "http://10te47kl27611.vicp.fun:19000")
             param("env.S3_AWS_REGION", "us-east-1")
-            password("env.AWS_ACCESS_KEY", "credentialsJSON:e2bf46ef-fcae-472d-a934-117a88db5241")
+            password("env.S3_ACCESS_KEY", "credentialsJSON:e2bf46ef-fcae-472d-a934-117a88db5241")
         }
     })
 })
