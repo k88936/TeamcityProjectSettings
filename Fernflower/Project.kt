f421a5fa44ced8283c9253dfba258b7131dfa479
k88936 <k88936@TeamCity>
2025-09-02 00:47:39 +0000

    Connection added (TeamCity change in 'Fernflower' project)

--- Fernflower/Project.kt
+++ Fernflower/Project.kt
@@ -4,6 +4,7 @@ import Fernflower.buildTypes.*
 import Fernflower.vcsRoots.*
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.projectFeatures.githubConnection
 
 object Project : Project({
     id("Fernflower")
@@ -13,4 +14,13 @@ object Project : Project({
 
     buildType(Fernflower_Build)
     buildType(Fernflower_Deploy)
+
+    features {
+        githubConnection {
+            id = "PROJECT_EXT_2"
+            displayName = "GitHub.com"
+            clientId = "Ov23liYhLaN7cSZi2Yif"
+            clientSecret = "credentialsJSON:d6ad8db6-9b92-4f96-9fc9-e96e4abe8585"
+        }
+    }
 })
