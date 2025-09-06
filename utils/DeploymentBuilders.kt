c676e3be8b646292108e67136417200081229da0
kvtodev <kvtodev@outlook.com>
2025-09-06 19:15:59 +0800

    merge patches

--- utils/DeploymentBuilders.kt
+++ utils/DeploymentBuilders.kt
@@ -21,7 +21,7 @@ object DeploymentBuilders {
             this.artifactRules = "build/${executableName}.zip"
 
             params {
-                text("env.QT_INSTALL", qtInstallPath)
+                text("QT_INSTALL", qtInstallPath)
             }
 
             steps {
@@ -29,7 +29,7 @@ object DeploymentBuilders {
                     id = "build_qt"
                     scriptContent = """
                             call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-                            cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%env.QT_INSTALL%\lib\cmake\Qt6\qt.toolchain.cmake -G Ninja 
+                            cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%QT_INSTALL%\lib\cmake\Qt6\qt.toolchain.cmake -G Ninja 
                             cmake --build build --config $buildConfig
                         """.trimIndent()
                 }
@@ -40,7 +40,7 @@ object DeploymentBuilders {
                             rm -r build\${executableName}
                             mkdir build\${executableName}
                             cp build\${executableName}.exe build\${executableName}
-                            %env.QT_INSTALL%\bin\windeployqt.exe build\${executableName}\${executableName}.exe
+                            %QT_INSTALL%\bin\windeployqt.exe build\${executableName}\${executableName}.exe
                             cd build
                             7z a ${executableName}.zip ${executableName}\
                 """.trimIndent()
