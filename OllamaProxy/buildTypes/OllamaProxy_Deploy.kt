4b1da6a363794537339c610e44e7bf5ae32b3632
kvtodev <kvtodev@outlook.com>
2025-09-06 21:12:43 +0800

    update

--- OllamaProxy/buildTypes/OllamaProxy_Deploy.kt
+++ OllamaProxy/buildTypes/OllamaProxy_Deploy.kt
@@ -1,12 +1,37 @@
 package OllamaProxy.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
 import utils.DeploymentBuilders
 
 
 object OllamaProxy_Deploy : BuildType({
     id ("OllamaProxy_Deploy")
     name ="Deploy"
+    
+    triggers {
+        finishBuildTrigger {
+            buildType = "OllamaProxy_BuildLinux"
+            successfulOnly = true
+        }
+        finishBuildTrigger {
+            buildType = "OllamaProxy_BuildWin"
+            successfulOnly = true
+        }
+    }
+
+    dependencies {
+        artifacts(OllamaProxy_BuildLinux) {
+            buildRule = lastSuccessful()
+            artifactRules = "*=>_deploy/"
+        }
+
+        artifacts(OllamaProxy_BuildWin) {
+            buildRule = lastSuccessful()
+            artifactRules = "*=>_deploy/"
+        }
+    }
+
     DeploymentBuilders.createGithubReleaseDeployment(
         tagPattern = "v%build.number%",
         generateNotes = true,
