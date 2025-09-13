79ea7f33bf0501561ad54f78520d52c575ed676b
kvtodev <kvtodev@outlook.com>
2025-09-13 15:25:39 +0800

    update

--- DockerProjects/CiContainers/Project.kt
+++ DockerProjects/CiContainers/Project.kt
@@ -2,6 +2,7 @@ package DockerProjects.CiContainers
 
 import DockerProjects.CiContainers.vcsRoots.*
 import DockerProjects.DockerBuildTemplate
+import jdk.internal.vm.ThreadContainers.root
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.Project
 
@@ -9,14 +10,16 @@ object Project : Project({
     id("CiContainers")
     name = "ci-containers"
 
-    buildType(build("base/yay/Dockerfile","kvtodev/ci-containers:builder-base"))
-    buildType(build("build/rust/Dockerfile","kvtodev/ci-containers:rust"))
-    buildType(build("build/flutter/Dockerfile","kvtodev/ci-containers:flutter"))
+    buildType(build("base/yay/Dockerfile", "kvtodev/ci-containers:builder-base"))
+    buildType(build("build/rust/Dockerfile", "kvtodev/ci-containers:rust"))
+    buildType(build("build/flutter/Dockerfile", "kvtodev/ci-containers:flutter"))
 
     vcsRoot(CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain)
 })
-fun build(dockerfilePath: String , imagename:String): BuildType{
+
+fun build(dockerfilePath: String, imagename: String): BuildType {
     return BuildType({
+        id("CiContainers_build" + imagename.replace("[^0-9a-zA-Z]".toRegex(), "_"))
         DockerBuildTemplate.createDockerBuild(
             dockerfilePath = dockerfilePath,
             imageName = imagename
