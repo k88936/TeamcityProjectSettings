040a2112f9ab43c4727adbbbb941910011622d8a
kvtodev <kvtodev@outlook.com>
2025-09-13 15:18:45 +0800

    update

--- DockerProjects/CiContainers/Project.kt
+++ DockerProjects/CiContainers/Project.kt
@@ -1,14 +1,29 @@
 package DockerProjects.CiContainers
 
-import DockerProjects.CiContainers.buildTypes.*
 import DockerProjects.CiContainers.vcsRoots.*
+import DockerProjects.DockerBuildTemplate
+import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.Project
 
 object Project : Project({
     id("CiContainers")
     name = "ci-containers"
 
-    buildType(CiContainers_Build)
+    buildType(build("base/yay/Dockerfile","kvtodev/ci-containers:builder-base"))
+    buildType(build("build/rust/Dockerfile","kvtodev/ci-containers:rust"))
+    buildType(build("build/flutter/Dockerfile","kvtodev/ci-containers:flutter"))
 
     vcsRoot(CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain)
 })
+fun build(dockerfilePath: String , imagename:String): BuildType{
+    return BuildType({
+        DockerBuildTemplate.createDockerBuild(
+            dockerfilePath = dockerfilePath,
+            imageName = imagename
+        )(this)
+        vcs {
+            root(CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain)
+        }
+    })
+
+}
