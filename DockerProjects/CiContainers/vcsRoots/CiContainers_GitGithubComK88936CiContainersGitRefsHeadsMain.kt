ca7abad646a7aeffdbe64d492e0f926a04d047b9
kvtodev <kvtodev@outlook.com>
2025-09-13 14:43:51 +0800

    update

--- DockerProjects/CiContainers/vcsRoots/CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain.kt
+++ DockerProjects/CiContainers/vcsRoots/CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain.kt
@@ -3,10 +3,12 @@ package DockerProjects.CiContainers.vcsRoots
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain : GitVcsRoot({
+    id("CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain")
     name = "ci-containers"
     url = "git@github.com:k88936/ci-containers.git"
     branch = "refs/heads/main"
-    branchSpec = """
-        refs/heads/*
-    """.trimIndent()
+    branchSpec = "refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "id_rsa"
+    }
 })
