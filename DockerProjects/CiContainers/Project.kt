8c0ce6fee2720e458f2d71364360f095e5426f7b
kvtodev <kvtodev@outlook.com>
2025-10-03 14:12:47 +0800

    remove Flutter build configuration from CI containers setup

--- DockerProjects/CiContainers/Project.kt
+++ DockerProjects/CiContainers/Project.kt
@@ -12,7 +12,6 @@ object Project : Project({
     buildType(build("base/yay/Dockerfile", "kvtodev/ci-containers:builder-base"))
     buildType(build("build/rust/Dockerfile", "kvtodev/ci-containers:rust"))
     buildType(build("build/java/Dockerfile", "kvtodev/ci-containers:java"))
-    buildType(build("build/flutter/Dockerfile", "kvtodev/ci-containers:flutter"))
 
     vcsRoot(CiContainers_GitGithubComK88936CiContainersGitRefsHeadsMain)
 })
