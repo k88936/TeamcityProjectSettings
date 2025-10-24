c0cf21e0eb9450f469d8fc7a07cd886866089a07
kvtodev <kvtodev@outlook.com>
2025-10-24 17:39:28 +0800

    add React Native demo project subproject

--- WebstormProjects/Project.kt
+++ WebstormProjects/Project.kt
@@ -1,7 +1,10 @@
 package WebstormProjects
+
 import jetbrains.buildServer.configs.kotlin.Project
+
 object Project : Project({
     id("WebstormProjects")
     name = "Webstorm Projects"
     subProject(WebstormProjects.GithubPages.Projects)
+    subProject(WebstormProjects.ReactNative.ReactNativeDemo.Project)
 })
