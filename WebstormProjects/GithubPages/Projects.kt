de4664f412b46ae7dce75c756f2f7f1bee3938d3
kvtodev <kvtodev@outlook.com>
2025-10-26 00:34:05 +0800

    update S3 credentials in source-of project configuration

--- WebstormProjects/GithubPages/Projects.kt
+++ WebstormProjects/GithubPages/Projects.kt
@@ -10,11 +10,11 @@ object Projects : Project({
     subProject(GithubPageTemplate("git@github.com:k88936/blogs.git"))
     subProject(GithubPageTemplate("git@github.com:k88936/source-of.git") {
         it.params {
-            password("env.S3_SECRET_KEY", "credentialsJSON:486eb30c-545f-49f3-b1b8-e4a07c95456a")
+            password("env.S3_ACCESS_KEY", "credentialsJSON:1b2416b2-6e73-4b71-84f0-8a658310e714")
+            password("env.S3_SECRET_KEY", "credentialsJSON:0344c592-9a94-405d-a1d0-7a6dc6ff5125")
             param("env.S3_BUCKET_NAME", "software-release")
             param("env.S3_ENDPOINT", "https://rustfs.k88936.top")
             param("env.AWS_REGION", "us-east-1")
-            password("env.S3_ACCESS_KEY", "credentialsJSON:23688bbb-1b91-46ba-b441-09702c979512")
         }
     })
 })
