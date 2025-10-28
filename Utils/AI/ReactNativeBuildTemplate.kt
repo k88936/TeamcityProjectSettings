e078bddc60adebef9d2370ebd7604aca72a0b769
kvtodev <kvtodev@outlook.com>
2025-10-28 22:11:40 +0800

    add Continue AI template and integrate into React Native demo build pipeline

---
+++ Utils/AI/ReactNativeBuildTemplate.kt
@@ -0,0 +1,34 @@
+package Utils.AI
+
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+
+object ContinueAITemplate {
+    fun createStep(
+        prompt: String,
+        workdir: String = ".",
+    ): BuildType.() -> Unit {
+        return {
+            steps {
+                script {
+                    id = "continue"
+                    scriptContent = """
+                        source /etc/profile
+                        cd $workdir
+                        cn --config config.yaml --verbose --auto -p "$prompt"
+                    """.trimIndent()
+                    dockerRunParameters = "--rm -v /root/.continue:/root/.continue"
+                    dockerImage = "kvtodev/ci-containers:continue"
+                    dockerPull = true
+                }
+            }
+            params {
+                password("env.CONTINUE_API_KEY", "")
+                param("env.CONTINUE_API_BASE", "https://llmapi.paratera.com/v1")
+                param("env.CONTINUE_MODEL_NAME", "Qwen3-Coder-Plus")
+                param("env.CONTINUE_CONTEXT_LENGTH", "1000000")
+            }
+        }
+    }
+}
--- WebstormProjects/ReactNative/ReactNativeDemo/buildTypes/ReactNativeDemo_Build.kt
+++ WebstormProjects/ReactNative/ReactNativeDemo/buildTypes/ReactNativeDemo_Build.kt
@@ -1,5 +1,6 @@
 package WebstormProjects.ReactNative.ReactNativeDemo.buildTypes
 
+import Utils.AI.ContinueAITemplate
 import WebstormProjects.ReactNative.ReactNativeBuildTemplate
 import WebstormProjects.ReactNative.ReactNativeDemo.vcsRoots.ReactNativeDemo_GitGithubComK88936reactNativeDemoGitRefsHeadsMain
 import jetbrains.buildServer.configs.kotlin.*
@@ -19,7 +20,6 @@ object ReactNativeDemo_Build : BuildType({
         root(ReactNativeDemo_GitGithubComK88936reactNativeDemoGitRefsHeadsMain)
     }
 
-
     triggers {
         vcs {
         }
@@ -30,5 +30,6 @@ object ReactNativeDemo_Build : BuildType({
         }
     }
 
+    ContinueAITemplate.createStep("say hello")(this)
     ReactNativeBuildTemplate.createReactNativeAndroidBuild()(this)
 })
