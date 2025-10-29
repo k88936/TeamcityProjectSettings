8c2499b0a6b1bbabfe7b024eda1e0eea16919197
kvtodev <kvtodev@outlook.com>
2025-10-29 12:35:54 +0800

    refactor VCS trigger rules logic and enhance exclusion functions

--- Utils/Trigger/TriggerTemplate.kt
+++ Utils/Trigger/TriggerTemplate.kt
@@ -1,27 +1,30 @@
 package Utils.Trigger
 
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 object TriggerTemplate {
-    fun excludeCI(): BuildType.() -> Unit {
+    private fun addVcsTriggerRule(rule: String): BuildType.() -> Unit {
         return {
-            triggers {
-                vcs {
-                    triggerRules += """-:comment=\[CI\]:**"""
+            (triggers.items.find { it is VcsTrigger } as? VcsTrigger)?.apply {
+                triggerRules += "\n$rule"
+            } ?: run {
+                triggers {
+                    vcs {
+                        triggerRules = rule
+                    }
                 }
             }
         }
     }
 
+    fun excludeCI(): BuildType.() -> Unit {
+        return addVcsTriggerRule("""-:comment=\[CI\]:**""")
+    }
+
     fun excludeUser(name: String): BuildType.() -> Unit {
-        return {
-            triggers {
-                vcs {
-                    triggerRules += """-:user=$name:**"""
-                }
-            }
-        }
+        return addVcsTriggerRule("-:user=$name:**")
     }
 
     fun excludeAI(): BuildType.() -> Unit {
