86d73653916c1d9e13b0ee0c375bf062fa4d3107
kvtodev <kvtodev@outlook.com>
2025-10-31 16:01:57 +0800

    update

--- RustroverProjects/Project.kt
+++ RustroverProjects/Project.kt
@@ -1,9 +1,11 @@
 package RustroverProjects
 
 import jetbrains.buildServer.configs.kotlin.Project
-object Project: Project({
+
+object Project : Project({
     id("RustroverProjects")
     name = "Rustrover Projects"
     subProject(RustroverProjects.OllamaProxy.Project)
+    subProject(RustroverProjects.TunetRust.Project)
     subProject(RustroverProjects.Gold.Project)
 })
