94922c14ece3133af958036c4a4797c2392d1c1b
kvtodev <kvtodev@outlook.com>
2025-10-31 16:07:52 +0800

    update

--- RustroverProjects/TunetRust/vcsRoots/TunetRust_GitGithubComK88936tunetRustGitRefsHeadsMaster.kt
+++ RustroverProjects/TunetRust/vcsRoots/TunetRust_GitGithubComK88936tunetRustGitRefsHeadsMaster.kt
@@ -6,7 +6,7 @@ object TunetRust_GitGithubComK88936tunetRustGitRefsHeadsMaster : GitVcsRoot({
     id("TunetRust_GitGithubComK88936tunetRustGitRefsHeadsMaster")
     name = "git@github.com:k88936/tunet-rust.git#refs/heads/master"
     url = "git@github.com:k88936/tunet-rust.git"
-    branch = "refs/heads/main"
+    branch = "refs/heads/master"
     branchSpec = "refs/heads/*"
     authMethod = uploadedKey {
         uploadedKey = "id_rsa"
