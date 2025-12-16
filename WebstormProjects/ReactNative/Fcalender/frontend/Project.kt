46c6660548f0379ab399cde7c3efd47c4b3346d9
kvtodev <kvtodev@outlook.com>
2025-12-16 05:01:42 +0000

    update

--- WebstormProjects/ReactNative/Fcalender/frontend/Project.kt
+++ WebstormProjects/ReactNative/Fcalender/frontend/Project.kt
@@ -1,6 +1,7 @@
 package WebstormProjects.ReactNative.Fcalender.frontend
 
 import WebstormProjects.ReactNative.Fcalender.frontend.buildTypes.FcalenderFrontendBuild
+import WebstormProjects.ReactNative.Fcalender.frontend.buildTypes.FcalenderFrontendTest
 import WebstormProjects.ReactNative.Fcalender.frontend.vcsRoots.FcalenderFrontendVCS
 import jetbrains.buildServer.configs.kotlin.Project
 
@@ -8,6 +9,7 @@ object Project : Project({
     name = "Fcalender Frontend"
     id("FcalenderFrontend")
 
+    buildType(FcalenderFrontendTest)
     buildType(FcalenderFrontendBuild)
 //    buildType(FcalenderFrontendApiCheck)
 //    buildType(FcalenderFrontendi18nCheck)
---
+++ WebstormProjects/ReactNative/Fcalender/frontend/buildTypes/FcalenderFrontendTest.kt
@@ -0,0 +1,58 @@
+package WebstormProjects.ReactNative.Fcalender.frontend.buildTypes
+
+import WebstormProjects.ReactNative.Fcalender.frontend.vcsRoots.FcalenderFrontendVCS
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildFeatures.notifications
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+object FcalenderFrontendTest : BuildType({
+    name = "Test"
+
+
+    vcs {
+        root(FcalenderFrontendVCS)
+    }
+
+
+    triggers {
+        vcs {
+            branchFilter = """
+                +:<default>
+                +:frontend-playground
+                +:dev
+            """.trimIndent()
+        }
+    }
+
+    features {
+        perfmon {}
+        notifications {
+            notifierSettings = emailNotifier {
+                email = "k88936@qq.com"
+            }
+            buildFailed = true
+        }
+    }
+    steps {
+        nodeJS {
+            id = "jest"
+            shellScript = $$"""
+                cd frontend
+                source /etc/profile
+                npm install
+                sdkmanager "platform-tools"
+                sdkmanager "cmdline-tools;latest"
+                sdkmanager "system-images;android-35;default;x86_64" "emulator"
+                $ANDROID_HOME/cmdline-tools/latest/bin/avdmanager create avd --name "pixel_9_api35" --device "pixel_9" --package "system-images;android-35;default;x86_64" --force
+                avd start-server
+                
+                npx detox build -c android.emu.release
+                npx detox test -c android.emu.release.ci --headless 
+            """.trimIndent()
+            dockerImage = "kvtodev/ci-containers:detox"
+            dockerPull = true
+        }
+    }
+})
