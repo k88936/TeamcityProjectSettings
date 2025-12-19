afa45f10500957043c8a23b02dbca3f7737b2d22
kvtodev <kvtodev@outlook.com>
2025-12-19 13:26:14 +0000

    Add Docker Registry connection feature

--- WebstormProjects/ReactNative/Fcalender/Project.kt
+++ WebstormProjects/ReactNative/Fcalender/Project.kt
@@ -4,6 +4,7 @@ import WebstormProjects.ReactNative.Fcalender.buildTypes.FcalenderBackendBuild
 import WebstormProjects.ReactNative.Fcalender.buildTypes.FcalenderFrontendBuild
 import WebstormProjects.ReactNative.Fcalender.vcsRoots.FcalenderVCS
 import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.projectFeatures.dockerRegistry
 
 object Project : Project({
     name = "Fcalender"
@@ -14,4 +15,12 @@ object Project : Project({
 //    buildType(FcalenderFrontendApiCheck)
 //    buildType(FcalenderFrontendi18nCheck)
     vcsRoot(FcalenderVCS)
+    features {
+        dockerRegistry {
+            id = "DOCKER_REGISTRY_CONNECTION"
+            name = "Docker Registry"
+            userName = "kvtodev"
+            password = "credentialsJSON:a43b4956-2b9e-457c-bf3c-6e04db2adc38"
+        }
+    }
 })
