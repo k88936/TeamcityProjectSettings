db562d488ae85e74ef3fd8fc250b8bea7cc3483d
kvtodev <kvtodev@outlook.com>
2025-12-25 15:35:50 +0000

    feat: add Scoop bucket deployment for local-router

--- GolandProjects/local-router/Project.kt
+++ GolandProjects/local-router/Project.kt
@@ -2,7 +2,9 @@ package GolandProjects.LocalRouter
 
 import GolandProjects.LocalRouter.buildTypes.LocalRouter_Build
 import GolandProjects.LocalRouter.buildTypes.LocalRouter_Deploy
+import GolandProjects.LocalRouter.buildTypes.LocalRouter_Scoop
 import GolandProjects.LocalRouter.vcsRoots.LocalRouter_GitGithubComK88936localRouterGitRefsHeadsMain
+import Utils.Deploy.Version.ScoopBucketVCS
 import jetbrains.buildServer.configs.kotlin.Project
 
 object Project : Project({
@@ -10,7 +12,9 @@ object Project : Project({
     name = "local-router"
 
     vcsRoot(LocalRouter_GitGithubComK88936localRouterGitRefsHeadsMain)
+    vcsRoot(ScoopBucketVCS)
 
     buildType(LocalRouter_Build)
     buildType(LocalRouter_Deploy)
+    buildType(LocalRouter_Scoop)
 })
---
+++ GolandProjects/local-router/buildTypes/LocalRouter_Scoop.kt
@@ -0,0 +1,47 @@
+package GolandProjects.LocalRouter.buildTypes
+
+import Utils.Deploy.Version.AppManifests
+import Utils.Deploy.Version.ScoopBucketDeployTemplate
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.FailureAction
+import jetbrains.buildServer.configs.kotlin.ReuseBuilds
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
+
+object LocalRouter_Scoop : BuildType(
+    {
+        triggers {
+            finishBuildTrigger {
+                buildType = LocalRouter_Deploy.id?.value
+                successfulOnly = true
+            }
+        }
+        dependencies {
+            snapshot(LocalRouter_Deploy) {
+                reuseBuilds = ReuseBuilds.NO
+                onDependencyFailure = FailureAction.CANCEL
+                onDependencyCancel = FailureAction.CANCEL
+                synchronizeRevisions = false
+            }
+        }
+        val version = "v%dep.LocalRouter_Deploy.build.number%"
+        val scoopManifests = AppManifests(
+            "local-router", """
+            {
+                "version": "$version",
+                "description": "local-router is a simple proxy to OpenAI compatible endpoint",
+                "license": "MIT",
+                "homepage": "https://github.com/k88936/local-router",
+                "url": "https://github.com/k88936/local-router/releases/download/$version/local-router.exe",
+                "bin": "local-router.exe",
+                "shortcuts": [
+                    [
+                        "local-router.exe",
+                        "local-router"
+                    ]
+                ]
+            } 
+        """.trimIndent()
+        )
+        ScoopBucketDeployTemplate.createDeployTemplate(scoopManifests)(this)
+    }
+)
