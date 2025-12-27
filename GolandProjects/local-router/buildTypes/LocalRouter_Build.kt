0d1265e73ef123c2662a7045978fe1b00ca46cd5
kvtodev <kvtodev@outlook.com>
2025-12-27 12:19:22 +0000

    feat: add Go test step and fix build paths in LocalRouter build

--- GolandProjects/local-router/buildTypes/LocalRouter_Build.kt
+++ GolandProjects/local-router/buildTypes/LocalRouter_Build.kt
@@ -52,8 +52,8 @@ object LocalRouter_Build : BuildType({
             name = "Build"
             scriptContent = """
                 source /etc/profile
-                CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build ./server -o bin/local-router.exe
-                go build ./server -o bin/local-router
+                CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/local-router.exe ./server 
+                go build -o bin/local-router ./server
             """.trimIndent()
             dockerImage = "kvtodev/ci-containers:go"
             dockerRunParameters =
