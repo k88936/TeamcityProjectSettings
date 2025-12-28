86c20066477268b70d40b8429afd58c259df75fe
kvtodev <kvtodev@outlook.com>
2025-12-28 20:34:24 +0000

    update

--- WebstormProjects/Project.kt
+++ WebstormProjects/Project.kt
@@ -7,5 +7,4 @@ object Project : Project({
     name = "Webstorm Projects"
     subProject(WebstormProjects.GithubPages.Projects)
     subProject (WebstormProjects.ReactNative.Fcalender.Project)
-    subProject(WebstormProjects.ReactNative.ReactNativeDemo.Project)
 })
