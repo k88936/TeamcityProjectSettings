47f8a83ace435de0727257b5bcecaf1fb3f06fc1
kvtodev <kvtodev@outlook.com>
2026-01-06 05:41:58 +0800

    feat: Add GitVcsRootTemplate utility for VCS root configuration

---
+++ utils/vcs/GitVcsRootTemplate.kt
@@ -0,0 +1,23 @@
+package utils.vcs
+
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot.AgentCleanFilesPolicy
+
+fun GitVcsRootTemplate(
+    url: String,
+    branch: String = "refs/heads/main",
+    branchSpec: String = "refs/heads/*",
+    sshKeyId: String = "id_rsa",
+    cleanPolicy: AgentCleanFilesPolicy = AgentCleanFilesPolicy.IGNORED_ONLY
+): GitVcsRoot {
+    return GitVcsRoot({
+        this.name = "$url#$branch"
+        this.url = url
+        this.branch = branch
+        this.branchSpec = branchSpec
+        agentCleanFilesPolicy = cleanPolicy
+        authMethod = uploadedKey {
+            uploadedKey = sshKeyId
+        }
+    })
+}
