236f975dc8f2d5dc701ee9b3da6f8d1be860c228
kvtodev <kvtodev@outlook.com>
2026-01-24 23:53:13 +0800

    Add build trigger dependency on Wallence config generation build

--- DockerProjects/Wallence/buildTypes/Wallence_Build_container.kt
+++ DockerProjects/Wallence/buildTypes/Wallence_Build_container.kt
@@ -3,7 +3,7 @@ package DockerProjects.Wallence.buildTypes
 import DockerProjects.Wallence.vcsRoot
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
 import utils.docker.applyDockerBuildSteps
 
 object Wallence_Build_container : BuildType({
@@ -16,7 +16,10 @@ object Wallence_Build_container : BuildType({
     }
 
     triggers {
-        vcs { }
+        finishBuildTrigger {
+            buildType = Wallence_Build_config_gen.id?.value
+            successfulOnly = true
+        }
     }
     features {
         perfmon { }
