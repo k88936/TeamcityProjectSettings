ddd159806a52764c98fbb3fbc54389e80d69ea23
kvtodev <kvtodev@outlook.com>
2026-02-22 12:08:42 +0800

    update

--- WebstormProjects/GithubPages/Projects.kt
+++ WebstormProjects/GithubPages/Projects.kt
@@ -10,8 +10,8 @@ object Projects : Project({
     subProject(GithubPageTemplate("git@github.com:k88936/blogs.git"))
     subProject(GithubPageTemplate("git@github.com:k88936/source-of.git") {
         it.params {
-            password("env.S3_ACCESS_KEY", "credentialsJSON:1b2416b2-6e73-4b71-84f0-8a658310e714")
-            password("env.S3_SECRET_KEY", "credentialsJSON:0344c592-9a94-405d-a1d0-7a6dc6ff5125")
+            password("env.S3_ACCESS_KEY", "%KVTO_RUSTFS_ACCESS_KEY%")
+            password("env.S3_SECRET_KEY", "%KVTO_RUSTFS_SECRET_KEY%")
             param("env.S3_BUCKET_NAME", "software-release")
             param("env.S3_ENDPOINT", "https://rustfs.k88936.top")
             param("env.AWS_REGION", "us-east-1")
