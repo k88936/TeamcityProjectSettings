36bdddfe72b12dcae63d6a7aae9c2ff79930ce23
kvtodev <kvtodev@outlook.com>
2026-03-31 18:35:52 +0800

    update

--- RustroverProjects/Project.kt
+++ RustroverProjects/Project.kt
@@ -5,8 +5,5 @@ import jetbrains.buildServer.configs.kotlin.Project
 object Project : Project({
     id("RustroverProjects")
     name = "Rustrover Projects"
-    subProject(RustroverProjects.OllamaProxy.Project)
-    subProject(RustroverProjects.TunetRust.Project)
-    subProject(RustroverProjects.Gold.Project)
     subProject(RustroverProjects.Citizen.Project)
 })
--- ScoopBucket/Project.kt
+++ ScoopBucket/Project.kt
@@ -1,16 +1,10 @@
 package ScoopBucket
 
-import ClionProjects.Qt.shotmd.buildTypes.Shotmd_Scoop
-import GolandProjects.LocalRouter.buildTypes.LocalRouter_Scoop
 import jetbrains.buildServer.configs.kotlin.Project
 import utils.deploy.version.ScoopBucketVCS
 
 object Project : Project({
     id("ScoopBucket")
     name = "ScoopBucket"
-
     vcsRoot(ScoopBucketVCS)
-
-    buildType(LocalRouter_Scoop)
-    buildType(Shotmd_Scoop)
 })
