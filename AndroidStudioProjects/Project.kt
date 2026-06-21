88679509af50d8388a9f5be38bfd02d37a09002f
kvtodev <kvtodev@outlook.com>
2026-06-21 15:58:26 +0800

    del NextcloudTV

--- AndroidStudioProjects/Project.kt
+++ AndroidStudioProjects/Project.kt
@@ -5,6 +5,4 @@ import jetbrains.buildServer.configs.kotlin.Project
 object Project : Project({
     id("AndroidStudioProjects")
     name = "Android Studio Projects"
-
-    subProject(AndroidStudioProjects.NextcloudTV.Project)
 })
