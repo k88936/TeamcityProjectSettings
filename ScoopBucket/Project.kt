7d90c85524a952024af748bf41af6461f73e514a
kvtodev <kvtodev@outlook.com>
2026-06-21 16:00:49 +0800

    del unused

--- ScoopBucket/Project.kt
+++ ScoopBucket/Project.kt
@@ -1,7 +1,5 @@
 package ScoopBucket
 
-import RustroverProjects.Citizen.buildTypes.Citizen_Scoop
-import RustroverProjects.Trackit.buildTypes.Trackit_Scoop
 import jetbrains.buildServer.configs.kotlin.Project
 import utils.deploy.version.ScoopBucketVCS
 
@@ -9,6 +7,4 @@ object Project : Project({
     id("ScoopBucket")
     name = "ScoopBucket"
     vcsRoot(ScoopBucketVCS)
-    buildType(Trackit_Scoop)
-    buildType(Citizen_Scoop)
 })
