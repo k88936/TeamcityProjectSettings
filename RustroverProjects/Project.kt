1ed78ae2d58f9189733e85765d776f03b9db0eaf
kvtodev <kvtodev@outlook.com>
2026-06-21 16:00:49 +0800

    del unused

--- RustroverProjects/Project.kt
+++ RustroverProjects/Project.kt
@@ -5,7 +5,5 @@ import jetbrains.buildServer.configs.kotlin.Project
 object Project : Project({
     id("RustroverProjects")
     name = "Rustrover Projects"
-    subProject(RustroverProjects.Citizen.Project)
-    subProject(RustroverProjects.Trackit.Project)
     subProject(RustroverProjects.Talkful.Project)
 })
